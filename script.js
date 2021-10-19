0b12ce9966facf2c446aee65c71d2d896479550f
Bogdanchikov-Ilya <63511922+Bogdanchikov-Ilya@users.noreply.github.com>
2021-10-19 17:35:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// Получение списка файлов
+async function getDownloads() {
+  const res = await fetch('https://cw60005.tmweb.ru/downloads/', {
+    method: 'GET'
+  })
+  let result = await res.json();
+  console.log(result)
+  result.forEach((item) => {
+    document.querySelector('.download').insertAdjacentHTML('beforeend', `
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Файл: <span>${item.filename}</span></h5>
+
+        <a href="https://cw60005.tmweb.ru/getfile?filename=${item.filename}" id="btn" class="btn btn-primary">Скачать</a>
+      </div>
+    </div>`)
+  })
+}
+getDownloads()
+
+// отправка файла
+document.querySelector('form').addEventListener('submit', async function (e) {
+  e.preventDefault()
+
+  let formData = new FormData();
+  formData.append("file", document.querySelector('.form-control-file').files[0]);
+  console.log(formData.get('file'))
+  const res = await fetch('https://cw60005.tmweb.ru/upload', {
+    method: 'POST',
+    body: formData
+  })
+  console.log(res)
+  if(res.status == 200 || res.status == 201) {
+    alert('Файл отправлен!')
+  }else if(res.status = 409){
+    alert('Такой файл уже существует')
+  }
+  else {
+    alert('Ошибк отправки файла!')
+  }
+  document.querySelector('.download').innerHTML = '';
+  document.getElementById("form").reset();
+  getDownloads();
+})
